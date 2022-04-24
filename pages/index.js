6eb69d4478cb779d714115f65efc11ad82fab8f5
Sadia <sadiashah059@gmail.com>
2022-04-24 06:51:37 -0200

    contact page issue

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import CustomLayout from '../src/components/layouts/customLayout'
 import CustomCarousel from '../src/components/ui/support/CustomCarousel'
 import InfoCard from '../src/components/ui/support/InfoCard'
 import Categories from '../src/components/ui/support/Categories'
+import DefineStep from '../src/components/ui/support/DefineStep'
+import ContactUS from '../src/components/ui/support/ContactUs'
 
 export default function Home() {
   return<CustomLayout>
@@ -9,6 +11,10 @@ export default function Home() {
     <InfoCard/>
 
     <Categories />
+
+    <DefineStep/>
+
+    
 
   </CustomLayout>
 }
