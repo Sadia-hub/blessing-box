794a108ff7e74cead83afe0910ad40639e5925cc
Sadia <sadiashah059@gmail.com>
2022-06-05 13:14:19 -0200

    Update contactpage.js

--- pages/contactpage.js
+++ pages/contactpage.js
@@ -2,7 +2,7 @@ import ContactUs from '../src/components/ui/support/ContactUs';
 import CustomLayout from '../src/components/layouts/customLayout';
 const ContactPage = () =>{
     return ( <CustomLayout>
-    <ContactUs/>
+            <ContactUs/>
     </CustomLayout>
     );
 }
