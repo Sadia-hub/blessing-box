86a98400ef00d1c5e213791cc5bffdc5cc4f888d
Sadia <sadiashah059@gmail.com>
2022-08-11 19:09:18 -0200

    code merged

--- src/components/ui/support/ProjectCard.js
+++ src/components/ui/support/ProjectCard.js
@@ -80,30 +80,7 @@ targetAmount="9000"
     setVisible(false);
   };
 
-  const handleDonate = (projectId) =>{
-    fetch("http://localhost:8080/create-checkout-session", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          items: [
-            { id: 1, quantity: 3 },
-            { id: 2, quantity: 1 },
-          ],
-        }),
-      })
-        .then(res => {
-          if (res.ok) return res.json()
-          return res.json().then(json => Promise.reject(json))
-        })
-        .then(({ url }) => {
-          window.location = url
-        })
-        .catch(e => {
-          console.error(e.error)
-        })
-  }
+  
 
     return<div>
 
