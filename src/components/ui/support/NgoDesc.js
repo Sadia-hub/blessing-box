3738bb5c0814265b2c86384dbcd79e726fba99be
Sadia <sadiashah059@gmail.com>
2022-08-12 08:13:49 -0200

    done with displaay ngo Details for user

--- src/components/ui/support/NgoDesc.js
+++ src/components/ui/support/NgoDesc.js
@@ -6,26 +6,56 @@ import ProjectCard from './ProjectCard';
 import NgoSidebarUser from './NgoSidebarUser';
 import LeftMenu from '../../layouts/NgosNavbar';
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react';
+import apiCall from './apiCall';
+import DisplayEditorContent from './DisplayEDitorContent';
 
 const NgoDesc = () => {
+
     const { Title, Paragraph } = Typography;
     const router = useRouter()
     const { category, id } = router.query
-    const myState = useSelector((state)=> state.detailsReducer);
-    const ngos = myState[category].filter((ngo)=>ngo.ngo.id==id)
-    console.log(ngos[0].ngo.name);
-    // sajna portion
+
+    const [ngoDetail, setNgoDetail] = useState({});
+    const [projects, setProjects] = useState([])
+    const [ngo, setNgo] = useState({})
+
+    //fetching ngo details for donor 
+    useEffect(()=>{
+        
+        apiCall(`ngo/${id}`,null, "GET", null, null)
+        .then((res)=>{
+        console.log(res)
+        if(res.success){
+                setNgo(()=>res.ngo)
+
+                if(res.ngoDetail){
+                   setNgoDetail(()=>res.ngoDetail)
+                }
+                if(res.projects){
+                    setProjects(()=>res.projects);
+                }      
+        }        
+        })
+        .catch((err)=>{
+            console.log(err.message)
+        })       
+    },[id]);
+
+
     function handleClick(e) {
         e.preventDefault();
         router.push('/ngosform');
     }
     const state = useSelector((state)=> state.userReducer);
     console.log("state is",state)
+
+
     return <CustomLayout>
         
         <div className={styles.container}>
             <Row align="middle" justify='center'>
-                <Title className={styles.title} ellipsis>{ ngos[0].ngo.name} </Title>
+                <Title className={styles.title} ellipsis>{ ngo.name} </Title>
             </Row>
             <Row justify='center'>
                 <LeftMenu />
@@ -33,52 +63,42 @@ const NgoDesc = () => {
             <Row justify="center" gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className={styles.container}>
 
                 <Col lg={{ span: 6 }} md={{ span: 6 }} sm={{ span: 24 }} className={styles.sideBar} justify="center">
-                    <NgoSidebarUser ngo={ngos[0].ngo} image={ngos[0].image}/>
+
+                    <NgoSidebarUser ngo={ngo} image={ngoDetail.image}/>
+
                 </Col>
                 
                 <Col lg={{ span: 18 }} md={{ span: 18 }} sm={{ span: 24 }} justify="center">
-                    <Row justify='center' id="about">
+                    <Row justify='center' id="about" >
+
                         <Title level={3} >About Us</Title>
-                        <br/>
-                        <br/>
-                        <Paragraph>
-                            
-                            {ngos[0].about_us}
-                           
-                        </Paragraph>
+                        <div style={{width:"95%"}}> <DisplayEditorContent value={ngoDetail.about_us}/></div>
+                       
+
                     </Row>
                     <Row justify='center' id="services">
+
                         <Title level={3}>Our Services</Title>
-                        <Paragraph>
-                        {ngos[0].services}  
-                        
-                        </Paragraph>
+                        <div style={{width:"95%"}}> <DisplayEditorContent value={ngoDetail.services}/></div>
+
                     </Row>
+
                     <Row justify='center' id="projects" style={{ margin: "5px" }}>
+
                         <Title level={3}>Projects</Title>
-                        <Paragraph>
-                        {ngos[0].projects}   
-                        </Paragraph>
+                        <div style={{width:"95%"}}> <DisplayEditorContent value={ngoDetail.projects}/></div>
                     </Row>
 
                     
                     
 
                     <Row justify="center">
-                        <ProjectCard />
-                        {/* <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard />
-                        <ProjectCard /> */}
-
-
+                        {
+                            projects.map((project)=>{
+                                return <ProjectCard />
+                            })
+                        }
+                        
                     </Row>
 
                 </Col>
